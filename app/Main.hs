8c95af18eda7fd3ac5054ae09aaec9bc5584be0d
Kylian Balan <kylian.balan@epitech.eu>
2020-04-21 14:56:44 +0200

    delete useless things adding "liste" file

--- app/Main.hs
+++ app/Main.hs
@@ -7,17 +7,15 @@ import Text.Read (readMaybe)
 import Data.Ord (Down(..))
 import Lib
 
--- Boostrap is below
-
 main :: IO ()
 main = do
     args <- getArgs
     case args of
         [file, pixelStr] -> do
             fileContents <- readFile file
-            let fileLines = lines fileContents
-            let fileMbPixels = fmap readMaybe fileLines
-            let mbFilePixels = sequence fileMbPixels
+            let fileLines = lines fileContents -- :: [String]
+            let fileMbPixels = fmap readMaybe fileLines -- :: [Maybe Pixel]
+            let mbFilePixels = sequence fileMbPixels -- :: Maybe [Pixel]
 
             let filePixels = fromMaybe (error "Bad file") mbFilePixels
             let pixel = fromMaybe (error "Wrong point argument") (readMaybe pixelStr)
@@ -27,18 +25,9 @@ main = do
 
 type Pixel = (Int, Int, Int)
 
-liste :: [Pixel]
-liste = [(33,18,109), (33,17,109), (35,18,111), (0, 0, 0), (10, 10, 10), (35,21,109), (38,21,112)]
-
 distance :: Pixel -> Pixel -> Float
 distance (x0, y0, z0) (x1, y1, z1) = sqrt (fromIntegral $ (x0 - x1)^2 + (y0 - y1)^2 + (z0 - z1)^2)
-
-readInputFile :: IO String
-readInputFile = head <$> getArgs >>= readFile
 
 closest :: [Pixel] -> Pixel -> Maybe Pixel
 closest l point = listToMaybe $ sortOn (distance point) l
 
-expelOutliers :: [Pixel] -> [Pixel]
-expelOutliers pixels = drop 1 (sortOn (Down . totalDistance) pixels)
-    where totalDistance pixel = sum (distance pixel <$> pixels)
