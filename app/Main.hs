10ac336b9b2f723f5f2851bc7ae4501f24b7343a
HeyShafty <nathan.lecorchet@epitech.eu>
2020-04-23 09:59:09 +0200

    Adding sum parsing

--- app/Main.hs
+++ app/Main.hs
@@ -7,13 +7,20 @@
 
 module Main where
 
-import System.Environment (getArgs)
-import Data.List (sortOn)
-import Data.Maybe (listToMaybe, fromMaybe)
-import Text.Read (readMaybe)
-import Data.Ord (Down(..))
-import DataTypes (Point, Color, Pixel, Cluster)
-import PixelCompressor (compressPixels)
+import           System.Environment             ( getArgs )
+import           Data.List                      ( sortOn )
+import           Data.Maybe                     ( listToMaybe
+                                                , fromMaybe
+                                                )
+import           Text.Read                      ( readMaybe )
+import           Data.Ord                       ( Down(..) )
+import           DataTypes                      ( Point(..)
+                                                , Color(..)
+                                                , Pixel(..)
+                                                , Cluster
+                                                )
+import           PixelCompressor                ( compressPixels )
+import           Data.Word                      ( Word8 )
 
 printClusters :: [Cluster] -> IO ()
 printClusters = error "Putain mais tamer"
@@ -22,32 +29,40 @@ main :: IO ()
 main = do
     args <- getArgs
     case args of
-        [file, pixelStr] -> do
-            fileContents <- readFile file
-            let fileLines = lines fileContents -- :: [String]
-            let fileMbPixels = fmap readMaybe fileLines -- :: [Maybe Pixel]
-            let mbFilePixels = sequence fileMbPixels -- :: Maybe [Pixel]
+        [nColorsStr, nLimitStr, file] -> do
+            let nColors = fromMaybe (error "Bad 'n' parameter") (readMaybe nColorsStr :: Maybe Int)
+            let nLimit = fromMaybe (error "Bad 'e' parameter") (readMaybe nLimitStr :: Maybe Float)
 
-            let filePixels = fromMaybe (error "Bad file") mbFilePixels
-            let pixel = fromMaybe (error "Wrong point argument") (readMaybe pixelStr)
+            mbPixels <- getPixelsFromFile file
+            let pixels = fromMaybe (error "Bad 'file' paramter") mbPixels
 
-            print $ closest filePixels pixel
+            print nColors
+            print nLimit
+            print pixels
+
+            -- let clusters = compressPixels pixels nColors nLimit
+
+            print "XD"
         _ -> error "Wrong arguments"
 
-type Pixelle = (Int, Int, Int)
+getPixelsFromFile :: FilePath -> IO (Maybe [Pixel])
+getPixelsFromFile file = do
+    fileContents <- readFile file
+    let fileLines    = lines fileContents
+    let fileMbPixels = fmap readPixel fileLines
+    return (sequence fileMbPixels)
 
--- liste :: [Pixel]
--- liste = [(33,18,109), (33,17,109), (35,18,111), (0, 0, 0), (10, 10, 10), (35,21,109), (38,21,112)]
-
-distance :: Pixelle -> Pixelle -> Float
-distance (x0, y0, z0) (x1, y1, z1) = sqrt (fromIntegral $ (x0 - x1)^2 + (y0 - y1)^2 + (z0 - z1)^2)
-
--- readInputFile :: IO String
--- readInputFile = head <$> getArgs >>= readFile
-
-closest :: [Pixelle] -> Pixelle -> Maybe Pixelle
-closest l point = listToMaybe $ sortOn (distance point) l
-
--- expelOutliers :: [Pixel] -> [Pixel]
--- expelOutliers pixels = drop 1 (sortOn (Down . totalDistance) pixels)
---     where totalDistance pixel = sum (distance pixel <$> pixels)
+-- (0, 0) (255, 255, 255)
+readPixel :: String -> Maybe Pixel
+readPixel line = do
+    let [pointStr, colorStr] = words line
+    -- xd <- case length pixelStr of
+    --     2 -> pixelStr
+    --     _ -> Nothing
+    -- let point = Point 0 0
+    -- let color = Color 0 0 0
+    let (px, py) = fromMaybe (error "Bad file format") (readMaybe pointStr :: Maybe (Int, Int))
+    let (cr, cg, cb) = fromMaybe (error "Bad file format") (readMaybe colorStr :: Maybe (Word8, Word8, Word8))
+    let point = Point px py
+    let color = Color cr cg cb
+    Just (Pixel point color)
--- src/DataTypes.hs
+++ src/DataTypes.hs
@@ -17,18 +17,18 @@ import Data.Word (Word8)
 data Point = Point {
     x :: Int,
     y :: Int
-}
+} deriving (Show)
 
 data Color = Color {
     r :: Word8,
     g :: Word8,
     b :: Word8
-}
+} deriving (Show)
 
 data Pixel = Pixel {
     piPoint :: Point,
     piColor :: Color
-}
+} deriving (Show)
 
 data Cluster = Cluster {
     clColor :: Color,
